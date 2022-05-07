81741e0a0ce4403e430c956308e79b8393e53c6e
torish14 <nanamin.104@icloud.com>
2022-05-08 05:43:44 +0900

    ⛏ chore: added config file to format with nimpretty

---
+++ util/pretty_files.nim
@@ -0,0 +1,26 @@
+# 再帰的に *.nim を探して、nimpretty の引数にファイル名を渡してフォーマット
+import os
+import strutils
+import osproc
+
+proc pretty_proc(startDir: string) =
+  for f in walkDirRec(startDir, yieldFilter = {pcFile}):
+    if f.endsWith(".nim"):
+      let process = startProcess("nimpretty", startDir, @["--backup:on", f],
+          nil, options = {poUsePath, poStdErrToStdOut})
+      echo f
+      discard process.waitForExit()
+
+when isMainModule:
+  let
+    cmdArgs = os.commandLineParams()
+    target_dir =
+      if cmdArgs.len == 1:
+        os.expandFilename(cmdArgs[0])
+      else:
+        os.expandFileName(".")
+
+  echo target_dir
+  pretty_proc(target_dir)
+else:
+  echo "not mainmodule"
